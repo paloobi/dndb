24d442e321c134c6e7976b4f5e714d5c88cc400b
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-09-12 11:21:01 -0700

    Added API routes and error handling

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '127.0.0.1'
 
 const server = http.createServer(async (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Server is running');
-    res.end();
+    console.log('Request received for ' + req.url);
+    if (req.url.startsWith('/api')) {
+        if (req.url === '/api/characters') {
+            console.log('characters requested');
+            res.end();
+        } 
+        
+        else {
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({message: 'API endpoint not found'}));
+        }
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Server is running');
+        res.end();
+    }
+
+    
 })
 
 server.listen(PORT, HOST, () => {
